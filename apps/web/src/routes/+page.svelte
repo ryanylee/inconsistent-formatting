a0bb027285feaca7f66c53aab7f9f910491ec96e
rylegames <rylee.nyc@gmail.com>
2024-03-15 13:22:31 -0400

    not working

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import "../main.css";
+</script>
+
+<h1 class="mr-1 mt-1 p-4 pt-2 text-sm font-bold">Welcome to SvelteKit</h1>
---
+++ prettier.config.js
@@ -0,0 +1,4 @@
+export default {
+  plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+  tailwindEntrypoint: "./apps/web/src/main.css",
+};
